bf2d4fd04b4747e53f2c7dcd974f81ccfc4a6765
Abdulrahman295 <movieshome160@gmail.com>
2024-02-11 07:22:13 +0200

    Add contentScript.js injection and bookmark button functionality

--- public/background.js
+++ public/background.js
@@ -1,5 +1,7 @@
 console.log("background.js loaded");
 
+// to do: check if the contentScript.js is injected or not before sending the message  
+
 chrome.runtime.onInstalled.addListener(async () => {
   for (const cs of chrome.runtime.getManifest().content_scripts) {
     for (const tab of await chrome.tabs.query({ url: cs.matches })) {
@@ -25,4 +27,26 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
       }
     );
   }
+});
+
+chrome.webNavigation.onCommitted.addListener(async (details) => {
+  if (details.transitionType === "reload" &&
+      details.url.includes("youtube.com/watch")) {
+        const videoUrl = new URL(details.url);
+        const urlParameters = new URLSearchParams(videoUrl.search);
+        console.log( "current tab is loaded with ID = " + details.tabId);
+
+        chrome.scripting.executeScript({
+          target: { tabId: details.tabId },
+          files: ["contentScript.js"],
+        }).then(() => {
+          chrome.tabs.sendMessage(
+            details.tabId,
+            { type: "NEW", videoId: urlParameters.get("v") },
+            (response) => {
+              if (response) console.log(response.message);
+            }
+          );
+        });
+      }
 });
--- public/contentScript.js
+++ public/contentScript.js
@@ -12,7 +12,24 @@ chrome.runtime.onMessage.addListener((obj, sender, sendResponse) => {
   }
 });
 
-const newVideoLoaded = () => {
-  const bookmarkBtn = document.getElementsByClassName("bookmark-btn")[0];
-  console.log(bookmarkBtn);
+const newVideoLoaded = async () => {
+  let bookmarkBtn = document.getElementsByClassName("bookmark-btn")[0];
+  if (!bookmarkBtn) {
+    // create new btn
+    bookmarkBtn = document.createElement("img");
+    bookmarkBtn.src = chrome.runtime.getURL("public/ext-icon.png");
+    bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+    bookmarkBtn.title = "Click here to create a bookmark";
+    console.log(bookmarkBtn);
+
+    // add new btn to the left controls
+    leftControls = document.getElementsByClassName("ytp-left-controls")[0];
+    player = document.getElementsByClassName("video-stream")[0];
+    leftControls.appendChild(bookmarkBtn);
+    bookmarkBtn.addEventListener("click", addNewBookmark);
+  }
+};
+
+const addNewBookmark = () => {
+  console.log("bookmark btn clicked");
 };
